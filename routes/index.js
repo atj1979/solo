429053abfc1d5720ffa7d119f1f28c833602ebba
Anthony Johnson <atj1979@gmail.com>
2015-03-09 15:23:30 -0700

    The server runs without errors.  Added some Firebase Items.  Starte some index.ejs stuff.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var Firebase = require('firebase');
+
+var fb = new Firebase("https://fiery-fire-7461.firebaseio.com");
+
+
+fb.set({ name: "Alex Wolfe" });
+fb.on("value", function(data) {
+  var name = data.val() ? data.val().name : "";
+  // alert("My name is " + name);
+});
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'March Pool' });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/users', function(req, req, next){
+  res.send('something goes here')
+})
+
 module.exports = router;

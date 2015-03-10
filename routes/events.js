7d5a5fcfdd017d0f2764cfb2ecb278e034c4e561
Anthony Johnson <atj1979@gmail.com>
2015-03-09 21:28:24 -0700

    Working on getting database to show.

---
+++ routes/events.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var Firebase = require('firebase');
+
+var fb = new Firebase("https://fiery-fire-7461.firebaseio.com");
+
+fb.on("value", function(data) {
+  var name = data.val() ? data.val().name : "";
+  var meeting = data.val() ? data.val.meeting : ""
+  render;
+});
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,37 @@
 var express = require('express');
 var router = express.Router();
 var Firebase = require('firebase');
-
+var app =express();
 var fb = new Firebase("https://fiery-fire-7461.firebaseio.com");
-
-
-fb.set({ name: "Alex Wolfe" });
-fb.on("value", function(data) {
-  var name = data.val() ? data.val().name : "";
-  // alert("My name is " + name);
-});
 
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'March Pool' });
+  // console.log('the requset is here ', req);
+
+});
+
+router.post('/', function(req, res, next){
+
+  fb.push({
+    name: req.body['input-name'],
+    meeting: req.body['input-meeting'],
+    destination: req.body['input-destination'],
+    time: req.body['input-time'],
+    address: req.body['input-address'],
+    phone: req.body['input-phone']
+  });
+ 
+  res.render('index', { title: 'A post request was sent'});
+});
+
+
+fb.on("value", function(data) {
+  var name = data.val() ? data.val().meeting : "";
+  var time = data.val() ? data.val().time : "";
+  var destination = data.val() ? data.val().destination : "";
 });
 
 module.exports = router;
